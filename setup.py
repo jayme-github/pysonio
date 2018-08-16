6e90b5f0d71a48ebb4bd9f3412c21cd669c01ea7
jayme-github <tuxnet@gmail.com>
2018-08-16 19:20:07 +0200

    Fix version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='pysonio',
-    version='0.1',
+    version='0.0.1',
     description='incomplete library to personio time tracking',
     long_description=long_description,
     long_description_content_type='text/markdown',
