6342b12a5532aae8d35d47f839e920d0f69830e3
jayme-github <tuxnet@gmail.com>
2018-08-16 20:19:37 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='pysonio',
-    version='0.0.2',
+    version='0.0.3',
     description='incomplete library to personio time tracking',
     long_description=long_description,
     long_description_content_type='text/markdown',
