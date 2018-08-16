41a4caa5567b0161834fead33b9f3b32c67e9859
jayme-github <tuxnet@gmail.com>
2018-08-16 14:42:54 +0200

    Add travis build any pypi release

---
+++ README.md
@@ -0,0 +1,13 @@
+pysonio
+=======
+
+[pysonio](https://github.com/jayme-github/pysonio) is a python library
+that provides minimalistic (and incomplete as of now) access to
+time-tracking data in Personio.
+
+Installation
+------------
+
+```
+pip install git+https://github.com/jayme-github/pysonio.git
+```
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+import setuptools
 
 
 def parse_requirements(path):
@@ -8,24 +8,28 @@ def parse_requirements(path):
         return [l.strip() for l in infile.readlines()]
 
 
-setup(
+with open("README.md", "r") as fh:
+        long_description = fh.read()
+
+setuptools.setup(
     name='pysonio',
     version='0.1',
     description='incomplete library to personio time tracking',
-    long_description=open('README.rst', 'r').read(),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     platforms=['any'],
     keywords='personio',
     author='Jayme',
     author_email='tuxnet@gmail.com',
     url='https://github.com/jayme-github/pysonio',
-    packages=['pysonio'],
+    packages=setuptools.find_packages(),
     license='GNU Affero General Public License v3',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU Affero General Public License v3',
         'Operating System :: OS Independent',
-        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
     ],
     install_requires=parse_requirements('requirements.txt'),
     # tests_require=parse_requirements('requirements-test.txt'),
